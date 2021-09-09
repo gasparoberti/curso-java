e71a3a9597317b24aa2661747b886b24ac8c67cf
Gaspar <obertigaspar@gmail.com>
2021-09-08 23:36:55 -0300

    se agregó la función login que verifica mediante un método post si el usuario existe en la bd. si existe redirecciona la página al listado de usuarios. Falta usar tokens para que realmente sea un login

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -28,8 +28,26 @@ public class UsuarioDaoImp implements UsuarioDao {
         entityManager.remove(usuario);
     }
 
+
     @Override
     public void postUsuario(Usuario usuario) {
         entityManager.merge(usuario);
     }
+
+    @Override
+    public boolean verificarEmailPassword(Usuario usuario) {
+        String query = "FROM Usuario where email = :email and password = :password";
+
+        List<Usuario> usuarioList = entityManager.createQuery(query)
+                .setParameter("email", usuario.getEmail())
+                .setParameter("password", usuario.getPassword())
+                .getResultList();
+
+//        return !usuarioList.isEmpty();    //es lo mismo
+        if (usuarioList.isEmpty())
+            return false;
+        else
+            return true;
+    }
+
 }
